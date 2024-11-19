3f4bc939d8cf5e9241a26e738262ef8ae2514cc9
imjustjayk <brycekrause@icloud.com>
2024-11-19 15:14:23 -0600

    locate days of contribution

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ parent = driver.find_element(By.CLASS_NAME, 'js-calendar-graph')
 children = parent.find_elements(By.CSS_SELECTOR, "*")
 for child in children:
     if child.tag_name == "tool-tip":
-        print(f"{child.get_attribute('for')}: {child.text}")
+        if "No " in child.text:
+            pass
+        else:
+            print(f"{child.get_attribute('for')}: {child.text}")
+
 
 driver.quit()
