3a788df362dd306173d66ae0637783b686843463
imjustjayk <brycekrause@icloud.com>
2024-11-19 14:21:35 -0600

    changes

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import requests 
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+from selenium import webdriver 
+
 url = "https://github.com/brycekrause" # Replace 'username' with the GitHub username 
 response = requests.get(url) 
 html_content = response.text 
 
 soup = BeautifulSoup(html_content, 'html.parser') # Extract the number of repositories 
 
-repo_count = soup.find('span', {'class': 'Counter'}).text 
-print(f"Number of repositories: {repo_count}")
+#element = soup.find('span', {'class': 'ml-2'})
+#element = soup.find('td', {'class': 'ContributionCalendar-day'})
+
+parent = soup.find('tbody')
+#elements = soup.find_all(class_= 'ContributionCalendar-day')
+#attributes = element.attrs
+
+if parent:
+    print('parent found')
+    children = parent.find_all(class_= 'ContributionCalendar-day')
+    for element in children:
+        print(element)
+#print(attributes)
