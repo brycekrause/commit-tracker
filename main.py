b16fee88bbb806fae6ec46600bb5c23443fd9e95
imjustjayk <brycekrause@icloud.com>
2024-11-19 15:36:08 -0600

    major improvements

--- main.py
+++ main.py
@@ -11,10 +11,24 @@ driver.implicitly_wait(5)
 
 parent = driver.find_element(By.CLASS_NAME, 'js-calendar-graph') 
 children = parent.find_elements(By.CSS_SELECTOR, "*")
+streak = 0
 for child in children:
     if child.tag_name == "tool-tip":
         if "No " not in child.text:
-            print(f"{child.get_attribute('for')}: {child.text}")
+            element = child.get_attribute('for').split('-')
+            row = element[3]
+            col = element[4]
+            element_text = child.text.split(' ')
+            month = element_text[3]
+            day = element_text[4].rstrip('th.')
+            commits = element_text[0]
+            #all = {
+            #    {'date': date},
+            #    {'row': element[3]},
+            #    {'col': element[4]}
+            #}
+            print(f"{col}:{row}: {commits} commits on {month} {day}th")
+
 
 
 driver.quit()
